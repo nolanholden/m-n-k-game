50c3af4809040440e2c612d473dc0b572de2184a
Nolan Holden <SD@SL.C>
2017-05-24 20:43:04 -0400

    Add enum for representation of cell conditions: x,o,empty

---
+++ m-n-k-game/Cell.h
@@ -0,0 +1,21 @@
+#ifndef CELL
+#define CELL
+
+namespace mnkgame {
+namespace implementation {
+	// Represents the three possible conditions of a board cell.
+	enum Cell {
+		EMPTY,
+		X,
+		O
+	};
+	static char get_cell_char(const Cell cell) {
+		if (cell == Cell::EMPTY)
+			return ' ';
+		return cell == Cell::X ? 'X' : 'O';
+	}
+
+} // namespace implementation
+} // namespace tttsolver
+
+#endif // CELL
